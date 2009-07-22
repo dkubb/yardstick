5eeeefc6741cf77832f03e57b69ccf080aad2d4b
Dan Kubb <dan.kubb@gmail.com>
2009-07-22 01:45:16 -0700

    Moved YARD mix-ins into separate file

--- lib/yardstick.rb
+++ lib/yardstick.rb
@@ -48,30 +48,5 @@ end # module Yardstick
 $LOAD_PATH << Yardstick::ROOT + 'lib'
 
 require 'yardstick/core_ext/object'
+require 'yardstick/yard_ext'
 require 'yardstick/autoload'
-
-module YARD  #:nodoc: all
-  module CodeObjects
-    class MethodObject
-
-      # Return the docstring associated with the method
-      #
-      # @example
-      #   method_object.docstring  # => YARD::Docstring instance
-      #
-      # @return [YARD::Docstring]
-      #   the docstring for this method
-      #
-      # @api public
-      def docstring
-        # TODO: update to use super() once reek does not flag it as
-        #   a utility method
-        @docstring.extend(Yardstick::Method)
-      end
-
-      # TODO: create an object to wrap tags, and extend each tag object with
-      # the matching module, if one exists under Yardstick::Tag::*
-
-    end # class MethodObject
-  end # module CodeObjects
-end # module YARD
---
+++ lib/yardstick/yard_ext.rb
@@ -0,0 +1,25 @@
+module YARD  #:nodoc: all
+  module CodeObjects
+    class MethodObject
+
+      # Return the docstring associated with the method
+      #
+      # @example
+      #   method_object.docstring  # => YARD::Docstring instance
+      #
+      # @return [YARD::Docstring]
+      #   the docstring for this method
+      #
+      # @api public
+      def docstring
+        # TODO: update to use super() once reek does not flag it as
+        #   a utility method
+        @docstring.extend(Yardstick::Method)
+      end
+
+      # TODO: create an object to wrap tags, and extend each tag object with
+      # the matching module, if one exists under Yardstick::Tag::*
+
+    end # class MethodObject
+  end # module CodeObjects
+end # module YARD
