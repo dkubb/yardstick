cef0dad6d56e98e038cec159dde4d3d619e3fa42
Dan Kubb <dan.kubb@gmail.com>
2013-04-14 21:44:12 -0700

    Fix problems with RUBY_ENGINE under ruby 1.8

--- lib/yardstick/yard_ext.rb
+++ lib/yardstick/yard_ext.rb
@@ -3,12 +3,11 @@
 module YARD  #:nodoc: all
 
   # Test if JRuby head is being used
-  JRUBY        = RUBY_ENGINE == 'jruby'
-  JRUBY_HEAD   = JRUBY && JRUBY_VERSION >= '1.7.4.dev'
-  JRUBY_19MODE = JRUBY && RUBY_VERSION  >= '1.9'
+  JRUBY_19MODE = RUBY_VERSION >= '1.9' && RUBY_ENGINE == 'jruby'
+  JRUBY_HEAD   = JRUBY_19MODE && JRUBY_VERSION >= '1.7.4.dev'
 
   # Fix jruby-head to use the ruby 1.8 parser until their ripper port is working
-  Parser::SourceParser.parser_type = :ruby18 if JRUBY_HEAD && JRUBY_19MODE
+  Parser::SourceParser.parser_type = :ruby18 if JRUBY_HEAD
 
   # Extend docstring with yardstick methods
   class Docstring
