eab8c7167480c3f77473de882a7dcdeb35232229
Dan Kubb <dan.kubb@gmail.com>
2013-08-03 00:26:18 -0700

    Change to use the 18 parser for jruby in 1.9 mode

--- lib/yardstick/yard_ext.rb
+++ lib/yardstick/yard_ext.rb
@@ -4,9 +4,8 @@ module YARD  #:nodoc: all
 
   # Test if JRuby head is being used
   JRUBY_19MODE = RUBY_VERSION >= '1.9' && RUBY_ENGINE == 'jruby'
-  JRUBY_HEAD   = JRUBY_19MODE && JRUBY_VERSION >= '1.7.4.dev'
 
   # Fix jruby-head to use the ruby 1.8 parser until their ripper port is working
-  Parser::SourceParser.parser_type = :ruby18 if JRUBY_HEAD
+  Parser::SourceParser.parser_type = :ruby18 if JRUBY_19MODE
 
 end # module YARD
