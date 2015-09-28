6b03fff02a71c7040acb2e4f786456a9fef074b1
John Backus <jbackus@stanford.edu>
2015-09-27 20:05:54 -0700

    Kill `RuleConfig` default options mutations

--- spec/unit/yardstick/rule_config/enabled_for_path_predicate_spec.rb
+++ spec/unit/yardstick/rule_config/enabled_for_path_predicate_spec.rb
@@ -3,37 +3,37 @@
 require 'spec_helper'
 
 describe Yardstick::RuleConfig, '#enabled_for_path?' do
-  subject { described_class.new(options).enabled_for_path?(path) }
-
-  let(:options) { {} }
+  subject { instance.enabled_for_path?(path) }
 
   %w[Foo::Bar#baz Foo::Bar.baz].each do |method|
     let(:path) { method }
 
     context 'when no restrictions' do
+      let(:instance) { described_class.new }
+
       it { should be(true) }
     end
 
     context 'when disabled' do
-      before { options[:enabled] = false }
+      let(:instance) { described_class.new(enabled: false) }
 
       it { should be(false) }
     end
 
     context 'when excluded by exact path' do
-      before { options[:exclude] = [path] }
+      let(:instance) { described_class.new(exclude: [path]) }
 
       it { should be(false) }
     end
 
     context 'when excluded by class or module' do
-      before { options[:exclude] = ['Foo::Bar'] }
+      let(:instance) { described_class.new(exclude: ['Foo::Bar']) }
 
       it { should be(false) }
     end
 
     context 'when excluded by superclass or subclass' do
-      before { options[:exclude] = ['Foo', 'Foo::Bar::Sub'] }
+      let(:instance) { described_class.new(exclude: ['Foo', 'Foo::Bar::Sub']) }
 
       it { should be(true) }
     end
